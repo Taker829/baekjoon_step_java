341aeb8510bcbb1e5e84cfd1047fc08327a6de01
Taker829 <kt829@naver.com>
2021-08-26 03:07:06 +0900

    001_2557, Hello World with Java
    Java로 백준 1단계 2557번 문제 풀이 수행.

---
+++ Step 01/001_2557/Main.java
@@ -0,0 +1,11 @@
+//class 선언, 백준은 모든 클래스 명을 Main으로 해주어야 한다
+public class Main   {
+    // main 선언
+    // return 값이 없기 때문에 void
+    public static void main(String[] args)  {
+        // System.out.print <- 개행문자 없는 출력문
+        //System.out.print("Hello, World!");
+        // System.out.println <- 개행문자 포함 출력문(ln)
+        System.out.println("Hello World!");
+    }
+}
