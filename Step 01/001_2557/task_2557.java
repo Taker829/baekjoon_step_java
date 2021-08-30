49a32cdbea5ca19c1d6a8728eeb34791862fdb2b
Taker829 <kt829@naver.com>
2021-08-31 02:23:36 +0900

    task 10718 clear

--- Step 01/001_2557/task_2557.java
+++ Step 01/001_2557/task_2557.java
@@ -1,5 +1,5 @@
 //class 선언, 백준은 모든 클래스 명을 Main으로 해주어야 한다
-public class Main   {
+public class task_2557  {
     // main 선언
     // return 값이 없기 때문에 void
     public static void main(String[] args)  {
---
+++ Step 01/002_10718/task_10718.java
@@ -0,0 +1,16 @@
+public class task_10718   {
+    public static void main(String[] args)  {
+        /*
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+        */
+
+        // 반복문 활용을 위한 cycle변수 선언
+        int i = 0;
+
+        // for문 활용
+        for (i = 0; i < 2; i++) {
+            System.out.println("강한친구 대한육군");
+        }
+    }
+}
