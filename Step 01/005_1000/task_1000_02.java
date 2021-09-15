0e7956c128482ba5178d3e37ccd050699487b777
Taker829 <kt829@naver.com>
2021-09-15 21:18:29 +0900

    task 1000 clear, feat.JAVA

---
+++ Step 01/005_1000/task_1000_02.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class task_1000_02   {
+    public static void main(String[] args) throws IOException {
+        int a = System.in.read() - 48;
+		System.in.read();
+		int b = System.in.read() - 48;
+		System.out.println(a + b);
+    }
+}
